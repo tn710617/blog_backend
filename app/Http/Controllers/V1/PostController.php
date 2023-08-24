a180f6b7ba9a145686b23498db5f8a30e03dbcb9
Ray <tn710617@gmail.com>
2023-08-24 08:03:46 +0800

    [feat] changed fulltext search to fuzzy search

--- app/Http/Controllers/V1/PostController.php
+++ app/Http/Controllers/V1/PostController.php
@@ -132,7 +132,10 @@ class PostController extends Controller
             )
             ->when($input->has('search'),
                 function (Builder $postBuilder) use ($input) {
-                    $postBuilder->whereFullText(['post_title', 'post_content'], $input['search']);
+                $searchTerm = "%${input['search']}%";
+                $postBuilder->where('post_title', 'like', $searchTerm)
+                    ->orWhere('post_content', 'like', $searchTerm);
+//                    $postBuilder->whereFullText(['post_title', 'post_content'], $input['search']);
                 })
             ->when(!Auth::check(), fn(Builder $postBuilder) => $postBuilder->where('is_public', true))
             ->whereLocale($localeHelper->normalizeLocale(App::currentLocale()))
