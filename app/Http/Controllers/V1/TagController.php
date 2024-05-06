27369ed16dc7c1db5176b98955f5eec060c97a7f
Ray <tn710617@gmail.com>
2024-05-06 20:17:43 +0800

    [feat] added a new rule to query tags

--- app/Http/Controllers/V1/TagController.php
+++ app/Http/Controllers/V1/TagController.php
@@ -6,13 +6,21 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\TagCollection;
 use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TagController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        return TagCollection::make(Tag::orderByDesc('used_at')->get()->sortBy('tag_name'));
+        $builder = Tag::query();
+
+        if (!$request->user()) {
+            $builder = $builder->where('is_private', false);
+        }
+
+
+        return TagCollection::make($builder->orderByDesc('used_count')->orderBy('tag_name')->get());
     }
 
     public function indexPopularly()
