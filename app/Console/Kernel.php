a5cffd6e6a0b2607b71bbfe5cfc38abc76c396a5
Ray <tn710617@gmail.com>
2024-05-06 19:58:42 +0800

    [feat] revised tag used count sync period

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('tags:sync-used-count')->everyMinute();
+        $schedule->command('tags:sync-used-count')->twiceDaily();
     }
 
     /**
