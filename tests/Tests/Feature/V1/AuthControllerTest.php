ffbc76012f7e52008064828a5d658a1a4ced2f71
Ray <tn710617@gmail.com>
2024-06-05 20:54:39 +0800

    [test] fix test

--- tests/Tests/Feature/V1/AuthControllerTest.php
+++ tests/Tests/Feature/V1/AuthControllerTest.php
@@ -17,13 +17,26 @@ class AuthControllerTest extends TestCase
 
     public function test_can_get_to_be_signed_message()
     {
-        $response = $this->getJson(route('v1.user.auth.get-to-be-signed-message'));
+        $this->withoutExceptionHandling();
+
+        $expectation = [
+            'wallet_address' => Str::random()
+        ];
+
+        $response = $this->getJson(route('v1.user.auth.get-to-be-signed-message', [
+            'wallet_address' => $expectation['wallet_address']
+        ]));
 
         $response->assertOk();
+
         $response->assertJsonStructure([
             'data' => [
                 'to_be_signed_message'
             ]
+        ]);
+
+        $this->assertDatabaseHas('wallet_to_be_signed_messages', [
+            'wallet_address' => $expectation['wallet_address']
         ]);
     }
 
@@ -42,18 +55,26 @@ class AuthControllerTest extends TestCase
 
     public function test_only_user_with_admin_wallet_address_can_login()
     {
-        $message = $this->get(route('v1.user.auth.get-to-be-signed-message'))->json('data.to_be_signed_message');
+        $expectation = [
+            'wallet_address' => Str::random(42)
+        ];
 
-        Config::set('custom.admin_wallet_addresses', [Str::random(42)]);
+        $message = $this->get(route('v1.user.auth.get-to-be-signed-message', [
+            'wallet_address' => $expectation['wallet_address']
+        ]))->json('data.to_be_signed_message');
+
+        Config::set('custom.admin_wallet_addresses', [$expectation['wallet_address']]);
         $this->postJson(route('v1.user.auth.login.metamask'), [
             'address' => self::TEST_ETH_ADDRESS,
             'signature' => app(Web3Service::class)->signMessage($message, self::TEST_ETH_PRIVATE_KEY)
         ])->assertUnauthorized();
     }
 
-    public function test_a_user_can_login()
+    public function test_an_user_can_login()
     {
-        $message = $this->get(route('v1.user.auth.get-to-be-signed-message'))->json('data.to_be_signed_message');
+        $message = $this->get(route('v1.user.auth.get-to-be-signed-message', [
+            'wallet_address' => self::TEST_ETH_ADDRESS
+        ]))->json('data.to_be_signed_message');
 
         Config::set('custom.admin_wallet_addresses', [self::TEST_ETH_ADDRESS]);
         $this->postJson(route('v1.user.auth.login.metamask'), [
